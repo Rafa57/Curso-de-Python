5ecd40822f1a2ee37fc8ec56620d47acaaaff819
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-04 12:29:54 -0300

    aula6 terminada

---
+++ iniciante/aula6-ex.py
@@ -0,0 +1,17 @@
+numero = input('Digite um número inteiro: ')
+numero = int(numero)
+
+def verificar_numero():
+    
+    if numero % 2 == 0:
+        print('O número é par!')
+    else:
+        print('O número é ímpar!')
+
+def verificar_inteiro():
+
+    if numero != int:
+        print("O número digitado não é inteiro")
+
+verificar_inteiro()
+verificar_numero()
--- iniciante/aula6.py
+++ iniciante/aula6.py
@@ -2,13 +2,13 @@
 
 numero_str = input('dobrarei o número inteiro digitado: ')
 
-# print(f'o dobro de {numero_str} é {numero_float * 2:.2f}')
+print(f'o dobro de {numero_str} é {numero_float * 2:.2f}')
 
-# if numero_str.isdigit():
-#     numero_float = float(numero_str)
-#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}!!')
-# else:
-#     print('Isso não é um número inteiro')
+if numero_str.isdigit():
+    numero_float = float(numero_str)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}!!')
+else:
+    print('Isso não é um número inteiro')
 
 try:
     numero_float = float(numero_str)
@@ -19,3 +19,60 @@ except:
     print('Isso não é um número')
     
     # pass/... = reserva um espaço e retornam um valor nulo (é usado para continuar sem gerar erros)
+
+
+# BOAS PRÁTICAS 1
+'''
+CONSTANTE = não existem no python, porém, por convenção, uma variável scrita em maiúsculo é considerada uma constante
+
+IF COM MUITAS CONDIÇÕES É RUIM
+
+'''
+
+velocidade = 78
+local = 121
+
+# constantes
+RADAR_1 = 70
+LOCAL_1 = 120
+RADAR_RANGE = 1
+
+acima_do_limite_radar_1 = velocidade > RADAR_1
+
+passou_no_radar_1 = local >= (LOCAL_1 - RADAR_RANGE) and local <= (LOCAL_1 + RADAR_RANGE)
+
+multado_no_radar_1 = passou_no_radar_1 and acima_do_limite_radar_1
+
+if acima_do_limite_radar_1:
+    print('Velocidade do carro acima do limite do radar 1')
+
+if multado_no_radar_1:
+    print('Carro multado em radar 1')
+
+if passou_no_radar_1:
+    print('Carro passou em radar 1')
+
+# ID
+v1 = id('a')
+v2 = 'a'
+print(v1)
+print(id(v2))
+
+# Flags, is, is not e none
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
