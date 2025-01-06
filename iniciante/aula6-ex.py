8ddd5f92a3f97898f103a776e65e1cd62c30a4e9
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-06 12:52:25 -0300

    ex-6 concluido

--- iniciante/aula6-ex.py
+++ iniciante/aula6-ex.py
@@ -1,17 +1,77 @@
-numero = input('Digite um número inteiro: ')
-numero = int(numero)
 
-def verificar_numero():
+def verificar_inteiro(numero):
+
+    try:
+        numero != int(numero)
+        return numero
+
+    except ValueError:
+        print('O número digitado não é inteiro.')
+        return None
+
+def verificar_impar_par(valor):
     
-    if numero % 2 == 0:
+    valor = int(valor)
+    if valor % 2 == 0:
         print('O número é par!')
     else:
         print('O número é ímpar!')
 
-def verificar_inteiro():
+def verificar_hora_valida(hora):
+    try:
+        hora = int(hora)
+        return hora
 
-    if numero != int:
-        print("O número digitado não é inteiro")
+    except ValueError:
+        print('Digite somente números inteiros')
+        return None
+    
+def verificar_hora(hora):
 
-verificar_inteiro()
-verificar_numero()
+    hora = int(hora)
+    if hora >= 0 and hora <= 11:
+        print('Bom dia!')
+    elif hora >= 12 and hora <= 17:
+        print('Boa tarde!')
+    elif hora >= 18 and hora <= 23:
+        print('Boa noite!')
+    else:
+        print('Hora inválida.')
+
+def verificar_nome(nome):
+
+    if not nome.isalpha():
+        print('Digite apenas letras')
+        return None
+
+    tamanho = len(nome)
+
+    if tamanho > 1:
+        if tamanho <= 4:
+            print('Seu nome é curto')
+        elif tamanho > 4 and tamanho <= 6:
+            print('Seu nome é normal')
+        elif tamanho > 6:
+            print("Seu nome é muito grande.")
+    else:
+        print('Digite mais de uma letra.')
+
+# IMPAR OU PAR
+numero_digitado = input('Digite um número inteiro: ')
+numero = verificar_inteiro(numero_digitado)
+
+if numero is not None:
+    verificar_impar_par(numero)
+
+# HORA
+horario = input('Que horas são? ')
+hora = verificar_hora_valida(horario)
+
+if hora is not None:
+    verificar_hora(hora)
+
+# NOME
+nome_do_usuario = input('Digite o seu nome: ')
+
+if nome_do_usuario is not None: 
+    verificar_nome(nome_do_usuario)
