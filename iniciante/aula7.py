bb64114d1ea19e20c83eeb8576b1a9cf5ae5e595
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-10 19:03:33 -0300

    Update aula7.py

--- iniciante/aula7.py
+++ iniciante/aula7.py
@@ -24,14 +24,46 @@
 
 #     print(contador)
 
-qtd_linhas = 5
-qtd_colunas = 5
+# qtd_linhas = 5
+# qtd_colunas = 5
 
-linha = 1
+# linha = 1
 
-while linha <= qtd_linhas:
-    coluna = 1
-    while coluna <= qtd_colunas:
-        print(f'{linha=} {coluna=}')
-        coluna += 1
-    linha += 1
+# while linha <= qtd_linhas:
+#     coluna = 1
+#     while coluna <= qtd_colunas:
+#         print(f'{linha=} {coluna=}')
+#         coluna += 1
+#     linha += 1
+
+# calculadora
+print('--ESCOLHA UMA OPÇÃO--')
+print('1. Calcular\n2. Sair')
+opcao = input()
+
+if opcao == '1':
+    
+    while True:
+
+        num1 = input('Digite o primeiro número: ')
+        operador = input('''Escolha a operação (+  -  *  /): ''')
+        num2 = input('Digite o segundo número: ')
+
+        num1 = int(num1)
+        num2 = int(num2)
+
+        if operador == '+':
+            print(num1 + num2)
+        elif operador == '-':
+            print(num1 - num2)
+        elif operador == '*':
+            print(num1 * num2)
+        elif operador == '/':
+            print(num1 / num2)
+
+        sair = input('Quer sair? [s]: ').lower().startswith('s')
+        if sair is True:
+            print('Saiu')
+            break
+else:
+    print('Saiu')
