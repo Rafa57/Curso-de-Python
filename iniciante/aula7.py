614e75f597ee409e637cde8901998de393542323
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-11 11:34:47 -0300

    Update aula7.py

--- iniciante/aula7.py
+++ iniciante/aula7.py
@@ -1,11 +1,12 @@
 # tipos imutáveis: str, int, float, bool
 
-# string = 'rafael santos'
-# variavel = f'{string[:7]}BOB{string[8:]}'
-# # string[7] = 'Bob' # gera um erro
-# print(variavel)
-# print(variavel[7])
-# print(string.zfill(25))
+string = 'rafael santos'
+variavel = f'{string[:7]}BOB{string[8:]}'
+# string[7] = 'Bob' # gera um erro
+
+print(variavel)
+print(variavel[7])
+print(string.zfill(25))
 
 # LAÇOS DE REPETIÇÃO (loop)
 
@@ -13,30 +14,30 @@
 # operadores de atribuição
 # +=  -=  *=  /=  //=  **=  %=
 
-# contador = 0
-# while contador <= 5:
+contador = 0
+while contador <= 5:
 
-#     contador += 1
+    contador += 1
 
-#     if contador == 4:
-#         print('o quatro sumiu')
-#         continue
+    if contador == 4:
+        print('o quatro sumiu')
+        continue
 
-#     print(contador)
+    print(contador)
 
-# qtd_linhas = 5
-# qtd_colunas = 5
+qtd_linhas = 5
+qtd_colunas = 5
 
-# linha = 1
+linha = 1
 
-# while linha <= qtd_linhas:
-#     coluna = 1
-#     while coluna <= qtd_colunas:
-#         print(f'{linha=} {coluna=}')
-#         coluna += 1
-#     linha += 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
 
-# calculadora
+# calculadora (minha)
 print('--ESCOLHA UMA OPÇÃO--')
 print('1. Calcular\n2. Sair')
 opcao = input()
@@ -67,3 +68,44 @@ if opcao == '1':
             break
 else:
     print('Saiu')
+
+# Solução
+while True:
+    num1 = input('Digite um numero: ')
+    operacao = input('Digite um operador (+ - * /): ')
+    num2 = input('Digite outro numero: ')
+
+    num_valido = None # flag
+    operador = '+ - * /'
+
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        teste = float(teste)
+        num_valido = True
+    except:
+        num_valido = None
+
+    if num_valido is None:
+        print('Um ou ambos os números são inválidos.')
+        continue
+    if operacao not in operador:
+        print("Operador inválido")
+        continue
+    if len(operacao) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    if operacao == '+':
+        print(num1 + num2)
+    elif operacao == '-':
+        print(num1 - num2)
+    elif operacao == '*':
+        print(num1 * num2)
+    elif operacao == '/':
+        print(num1 / num2)
+
+    sair = input('Quer sair? [s]: ').lower().startswith('s')
+    if sair is True:
+        print('Saiu')
+        break
