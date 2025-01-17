d376aabdbb740b4956d1cc4ad3d84371d70a94fc
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-17 15:56:52 -0300

    Update ex-12.py

--- iniciante/ex-12.py
+++ iniciante/ex-12.py
@@ -1,6 +1,23 @@
-# cpf = '602.976.060-26'
 # import math
-cpf = '18786476017'
+import re
+import sys
+
+entrada = input('CPF: ')
+# cpf = '187.864.760-17'.replace('.', '').replace('-','')
+
+cpf = re.sub(
+    r'[^0-9]', '', # substitui os caracteres que não forem numeros por ''
+    entrada
+    )
+
+primeiro_char_repetido = entrada == entrada[0] * len(entrada)
+
+if primeiro_char_repetido:
+    print('caractere repetido')
+    sys.exit()
+
+# cpf_limpo = cpf.translate(str.maketrans('', '', '.-'))
+
 nove_digitos = cpf[:9]
 
 def multiplicar_cpf():
@@ -30,7 +47,7 @@ print(multiplicar_cpf())
 multi_result = 10 * somar_multi_cpf()
 resto = multi_result % 11
 verificacao_digito = 0 if resto > 9 else resto
-print(f'O digito 1 é: {verificacao_digito}')
+# print(f'O digito 1 é: {verificacao_digito}')
 
 # resultado = sum(lista_multi)
 # resultado = int(math.fsum(lista_multi))
@@ -38,7 +55,6 @@ print(f'O digito 1 é: {verificacao_digito}')
 
 
 # RESOLUÇÃO (PRIMEIRO DIGITO)-----------------------
-cpf = '18786476017'
 nove_digitos = cpf[:9]
 
 def multiplicar_cpf():
@@ -50,7 +66,7 @@ def multiplicar_cpf():
     
     return resultado
 
-resto1 = (multiplicar_cpf() * 10) % 11
+resto_1 = (multiplicar_cpf() * 10) % 11
 
 verificacao_digito = resto if resto <= 9 else 0
 print(f'O digito 1 é: {verificacao_digito}')
@@ -68,13 +84,14 @@ def multiplicar_cpf():
     
     return resultado
 
-resto2 = (multiplicar_cpf() * 10) % 11
+resto_2 = (multiplicar_cpf() * 10) % 11
 
-verificacao_digito = resto2 if resto2 <= 9 else 0
+verificacao_digito = resto_2 if resto_2 <= 9 else 0
 print(f'O digito 2 é: {verificacao_digito}')
 
-cpf_calculado = f'{nove_digitos}{resto1}{resto2}'
+cpf_calculado = f'{nove_digitos}{resto_1}{resto_2}'
 print(cpf_calculado)
 verificacao_cpf = 'CPF válido' if cpf == cpf_calculado else 'CPF inválido'
 print(verificacao_cpf)
 # --------------------------------------------------
+
