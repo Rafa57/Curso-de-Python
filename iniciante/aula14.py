15aab8498e3e5463cb8cb1d8b0efe4e9e640ea19
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-21 18:31:19 -0300

    Update aula14.py

--- iniciante/aula14.py
+++ iniciante/aula14.py
@@ -1,13 +1,16 @@
-# HIGHER ORDER FUNCTIONS (first class functions)
+# HIGHER ORDER FUNCTIONS -> Funções que podem receber e/ou retornar outras funções
+# FIRST-CLASS FUNCTIONS - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
 
-# def greeting(person, msg):
-#     return f'Hi {person}, {msg}'
+def greeting(person, msg):
+    return f'Hi {person}, {msg}'
 
-# def execute(function, *args):
-#     return function(*args)
+def execute(function, *args):
+    return function(*args)
  
-# print(execute(greeting, 'Lais', 'good afternoon.'))
+print(execute(greeting, 'Lais', 'good afternoon.'))
 
+
+# teste com lista (código pessoal)-----------------------
 def add(*args):
     lista = []
     lista.extend(args) # Itera sobre os elementos de args e os adiciona individualmente à lista.
@@ -17,3 +20,4 @@ def executar(function, *args):
     return function(*args)
 
 print(executar(add, 'nome', 'dia', 'mes', 'ano'))
+# -------------------------------------------------------
