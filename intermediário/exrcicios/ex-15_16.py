c761409497a953b965cc21bde14fde837289e93f
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-23 18:20:27 -0300

    ex-15_16 finalizado

---
+++ intermediário/exrcicios/ex-15_16.py
@@ -0,0 +1,33 @@
+# SISTEMA DE PERGUNTAS E RESPOSTAS
+
+questions = [
+    {
+        'Pergunta': 'Quanto é 12 + 24?',
+        'Opções': ['32', '38', '36', '40', '37'],
+        'Resposta': '36'
+    },
+    {
+        'Pergunta': 'Quanto é 6 * 5?',
+        'Opções': ['26', '45', '30', '65', '36'],
+        'Resposta': '30'
+    },
+    {
+        'Pergunta': 'Quanto é 81 / 9?',
+        'Opções': ['3', '19', '72', '90', '9'],
+        'Resposta': '9'
+    }
+]
+
+
+def answer(answer):
+    ...
+
+quest_1 = questions[0].get('Pergunta')
+quest_2 = questions[1].get('Pergunta')
+quest_3 = questions[2].get('Pergunta')
+
+print(quest_3)
+
+# aswr_1 = input('Digite a resposta')
+# aswr_2 = input('Digite a resposta')
+# aswr_3 = input('Digite a resposta')
