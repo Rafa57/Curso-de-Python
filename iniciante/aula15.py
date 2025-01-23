efaa1d28da462205021172b265108fb5583ebaba
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-23 17:51:26 -0300

    aula 16 finalizada

--- iniciante/aula15.py
+++ iniciante/aula15.py
@@ -43,26 +43,26 @@
 
 # # MANIPULANDO AS CHAVES E VALORES EM DICIONÁRIOS
 
-person_3 = {} # as chaves devem ser tipos imutáveis (int, float, strings, tuplas e valores booleanos.)
+# person_3 = {} # as chaves devem ser tipos imutáveis (int, float, strings, tuplas e valores booleanos.)
 
-# definindo chaves e valores de forma dinâmica
+# # definindo chaves e valores de forma dinâmica
 
-chave = 'nome' # dessa forma, só é necessário alterar a variável
-person_3[chave] = 'Rafael'
+# key = 'name' # dessa forma, só é necessário alterar a variável
+# person_3[key] = 'Rafael'
 
-person_3["idade"] = 29 # dessa forma, a alteração deve ser feita em todos os lugares que referenciam essa chave
+# person_3["age"] = 29 # dessa forma, a alteração deve ser feita em todos os lugares que referenciam essa chave
 
-print(person_3)
-print(person_3[chave], person_3['idade'])
+# print(person_3)
+# print(person_3[key], person_3['age'])
 
-del person_3['idade']
-print(person_3)
+# del person_3['age']
+# print(person_3)
 
-print(person_3.get('sobrenome', None)) # retorna None se a chave não existir (padrão)
+# print(person_3.get('surname', None)) # retorna None se a chave não existir (padrão)
 
-if person_3.get('sobrenome') is None:
-    print('A chave não existe')
-else:
-    print(person_3['sobrenome'])
+# if person_3.get('surname') is None:
+#     print('the key dont exist.')
+# else:
+#     print(person_3['surname'])
 
 # print(person_3['sobrenome'])
---
+++ iniciante/aula16.py
@@ -0,0 +1,75 @@
+# métodos uteis dos dicionários em Python
+
+import copy
+
+lang = {
+    'name': 'Python',
+    'cathegory': "Code",
+    'age': 40,
+    'list': [12, 23, 34]
+}
+
+# print(len(book)) # tamanho do dicionário
+
+# print(book.keys()) # retorna as chaves
+
+# print(book.values()) # retorna os valores
+
+# print(book.items()) # retorna as chaves e o valores dentro de tuplas (key, value)
+
+# book.setdefault('age', 56) # adiciona a chave : valor se ela não existir
+# print(book['age'])
+
+# shalow copy -> cópia rasa
+# book_2 = book_1.copy() # copia os valores imutáveis (não copia listas internas)
+
+# book_2 = copy.copy(book_1) # usando o módulo copy (também é uma cópia rasa)
+
+# book_2 = copy.deepcopy(book_1) # copia os subníveis também (listas internas)
+
+# book_2['age'] = 78
+# print(book_1)
+# print(book_2)
+# book_2['list'][2] = [56, 67, 78] # não copia, apenas aponta para a mesma lista em book_1
+# print(book_1)
+# print(book_2)
+
+# for value in book.values():
+#     print(value)
+
+# for key, value in book.items():
+#     print(f'{key}: {value}')
+
+# get
+print(lang.get('name')) # retorna o valor da chave (retorna None se não existir)
+
+print(lang.get('height', 'not found')) # o valor padrão pode ser mudado
+
+# pop
+# print(book1)
+# del_age = book1.pop('age') # apaga um item com a chave indicada
+# print(book1)
+
+# popitem
+print(lang)
+del_list = lang.popitem() # apaga a ultima chave do dicionario
+print(del_list) # retorna o par chave:valor removido
+print(lang)
+
+# Update
+lang.update({
+    'name': 'PHP',
+    'area': 'back-end'
+}) # atualiza o dicionário (adicionar, atualizar)
+print(lang)
+
+lang.update(name='js', area='front-end') # segunda forma
+print(lang)
+
+form_3 = ('name', 'java'), ('area', 'back-end') # deixar uma virgula no final da tupla se tiver somente uma ( ('key', 'value'), )
+lang.update(form_3)
+print(lang)
+
+form3_list = [['name', 'Kotlin'], ['area', 'android']]
+lang.update(form3_list)
+print(lang)
