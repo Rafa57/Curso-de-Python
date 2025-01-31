8c6213346953b7f11c5bdd0a98d0be3f67e6ad5e
Rafael S. Gomes <rsg5775@gmail.com>
2025-01-31 17:10:59 -0300

    Update ex-15_16.py

--- intermediário/exrcicios/ex-15_16.py
+++ intermediário/exrcicios/ex-15_16.py
@@ -18,38 +18,57 @@ questions = [
     }
 ]
 
-def answer(question):
-    def quest(aswr):
-        options = questions[question].get('Opções')
-        correct = questions[question].get('Resposta')
+# def answer(question):
+#     def quest(aswr):
+#         options = questions[question].get('Opções')
+#         correct = questions[question].get('Resposta')
 
-        if aswr not in options:
-           return 'Opção inválida.\n'
-        if aswr != correct:
-            return 'Resposta errada.\n'
-        else:
-            return 'Certa resposta.\n'
+#         if aswr not in options:
+#            return 'Opção inválida.\n'
+#         if aswr != correct:
+#             return 'Resposta errada.\n'
+#         else:
+#             return 'Certa resposta.\n'
 
-    return quest
+#     return quest
 
-def user_answer():
-    quest_1 = answer(0)
-    quest_2 = answer(1)
-    quest_3 = answer(2)
+# def user_answer():
+#     quest_1 = answer(0)
+#     quest_2 = answer(1)
+#     quest_3 = answer(2)
 
-    print(questions[0].get('Pergunta'))
-    for i in questions[0].get('Opções'):
-        print(i)
-    print(quest_1(input('Digite a resposta: ')))
+#     print(questions[0].get('Pergunta'))
+#     for i in questions[0].get('Opções'):
+#         print(i)
+#     print(quest_1(input('Digite a resposta: ')))
 
-    print(questions[1].get('Pergunta'))
-    for i in questions[1].get('Opções'):
-        print(i)
-    print(quest_2(input('Digite a resposta: ')))
+#     print(questions[1].get('Pergunta'))
+#     for i in questions[1].get('Opções'):
+#         print(i)
+#     print(quest_2(input('Digite a resposta: ')))
 
-    print(questions[2].get('Pergunta'))
-    for i in questions[2].get('Opções'):
-        print(i)
-    print(quest_3(input('Digite a resposta: ')))
+#     print(questions[2].get('Pergunta'))
+#     for i in questions[2].get('Opções'):
+#         print(i)
+#     print(quest_3(input('Digite a resposta: ')))
 
-user_answer()
+# user_answer()
+
+# RESOLUÇÃO
+
+for pergunta in questions:
+    print(pergunta['Pergunta'])
+
+    for i, opcao in enumerate(pergunta['Opções']):
+        # letra = chr(65 + i) # usa o código ASCII (A = 65)
+        print(f'{i}) {opcao}')
+        
+    resp_certa = pergunta['Resposta']
+    resp_usuario = input('Resposta: ')
+
+    if int(resp_usuario) < 0 and int(resp_certa) > len(pergunta):
+        print('Opção inválida.\n')
+    elif resp_usuario != resp_certa:
+         print('Resposta errada.\n')
+    else:
+        print('Resposta certa.\n')
