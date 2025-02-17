787fb2ede31071fa465e64e9ba0e180a8d9a6fc0
Rafael S. Gomes <rsg5775@gmail.com>
2025-02-17 10:11:48 -0300

    Update ex-17.py

--- intermediário/exrcicios/ex-17.py
+++ intermediário/exrcicios/ex-17.py
@@ -9,12 +9,15 @@ bidimensional_list = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [19, 72, 32, 41, 32, 32, 32, 5, 34, 56],
     [11, 32, 53, 48, 59, 60, 9, 9, 9, 102],
-    [1, 2, 3, 4, 5, 8, 8, 8, 8, 15],
+    [1, 2, 2, 4, 5, 8, 8, 8, 8, 15],
     [1, 2, 3, 7, 5, 7, 7, 8, 9, 10],
     [1, 2, 3, 4, 5, 5, 5, 8, 9, 102],
     [1, 2, 3, 2, 1, 2, 5, 2, 9, 10],
     [1, 2, 14, 4, 14, 2, 14, 8, 9, 102]
 ]
+
+def duplicados():
+    
 
 def repetitions():
     for i in bidimensional_list:
@@ -26,7 +29,7 @@ def repetitions():
                 counting = i.count(value)
 
                 if counting > 1:
-                    print(f'Linha: {i}\nValor: {value}\nRepetições: {counting}\n')
+                    print(f'Lista: {i}\nValor repetido: {value}\nRepetições: {counting}\n')
 
             checked.add(value)
 
