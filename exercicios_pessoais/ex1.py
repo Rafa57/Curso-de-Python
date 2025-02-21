ef78a2b2eb30d0f9ae97f22f9ee934a67abb4cf6
Rafael S. Gomes <rsg5775@gmail.com>
2025-02-21 18:49:19 -0300

    Update ex1.py

--- exercicios_pessoais/ex1.py
+++ exercicios_pessoais/ex1.py
@@ -13,8 +13,7 @@ print(fibonnaci(10))
 
 
 # PALÍNDROMO
-
-def palindromo(text):
+def palindrome(text):
     text = str(text).lower()
     final_txt = text[::-1]
 
@@ -23,15 +22,13 @@ def palindromo(text):
     else:
         return False
 
-print(palindromo('tenet'))
-print(palindromo('prime'))
+print(palindrome('tenet'))
+print(palindrome('prime'))
 
 
 # ORDENAÇÃO BUBBLE SORT
-
 bubble_list = [23, 34, 67, 22, 32, 1, 2, 43]
 # bubble_list.sort(reverse=True) # sort modifica a lista
-# print(bubble_list)
 
 def bubble_sort(array):
     return sorted(array) # cria uma cópia rasa
@@ -44,16 +41,33 @@ print(reverse_bubble_sort(bubble_list))
 
 
 # NUMEROS PRIMOS EM INTERVALO
+def prime_verify(num):
+    if num < 2:
+        return False
+        
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
 
-def prime_number(num1, num2):
-    number_list = []
+    return True
 
-    while num1 <= num2:
-        if num1 % 2 != 0 and num1 % 3 != 0 and num1 % 5 != 0:
-            number_list.append(num1)
-
-        num1 += 1
-
-    return number_list
+def prime_number(n1, n2):
+    return [n for n in range(n1, n2 + 1) if prime_verify(n)]
 
 print(prime_number(10, 50))
+
+
+# CONTAGEM DE CARACTERES
+def char_count(iter):
+    dictio = {}
+
+    for i in iter:
+        if i not in dictio:
+            counter = 0
+            dictio[i] = counter
+        if i in dictio:
+            dictio[i] += 1
+
+    return dictio
+
+print(char_count('hight'))
