834ae2dc65de87dad7e2ced2da65051ee22c6e7d
Rafael S. Gomes <rsg5775@gmail.com>
2025-04-15 10:29:30 -0300

    Update ex4.py

--- exercicios_pessoais/ex4.py
+++ exercicios_pessoais/ex4.py
@@ -23,8 +23,8 @@ print(somar_chaves(entrada))
 # Remover caractere repetido mantendo a primeira ocorrência
 
 def remove_dupli(text):
-
     clean_text = ''
+
     for char in text:
         clean_text += char if char not in clean_text else ''
 
@@ -32,16 +32,43 @@ def remove_dupli(text):
 
 print(remove_dupli('abacate'))
 print(remove_dupli('banana'))
+
+# resolução
+def remove_duplicata(text):
+    seen = set()
+    resultado = []
+
+    for char in text:
+        if char not in seen:
+            seen.add(char)
+            resultado.append(char)
+
+    return ''.join(resultado)
+
 # ---------------------------------------------------
 
 # Agrupar palavras por tamanho
 
-def group_words(words):
+def group_by_len(words):
     word_dic = {}
-    key = ''
-    for i in words:
-            ...
+
+    for word in words:
+        word_dic[len(word)] = [n for n in words if len(n) == len(word)]
     
     return word_dic
 
-print(group_words(["sol", "lua", "estrela", "céu", "mar", "universo"]))
+print(group_by_len(["sol", "lua", "estrela", "céu", "mar", "universo"]))
+
+# resolução
+def palavras_agupadas(words):
+    word_dic = {}
+
+    for word in words:
+        tamanho = len(word)
+
+        if tamanho not in word_dic:
+            word_dic[tamanho] = []
+
+        word_dic[tamanho].append(word)
+
+    return word_dic
